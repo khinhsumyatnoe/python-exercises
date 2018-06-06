3199399fd253e159380f9f259678e702d0393b64
khinhsumyatnoe <khinsumyatnoe771997@gmail.com>
2018-06-06 10:54:27 +0630

    Added ex4

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name='Zed A. Shaw'
+my_age=35
+my_height=74
+my_weight=180
+my_eyes='Blue'
+my_teeth='White'
+my_hair='Brown'
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height}pounds heavy.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actuall thatt's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teech are usually {my_teeth} depending on the coffee.")
+total=my_age+my_height+my_weight
+print("If I add {my_age}, {my_height}, {my_weight}, I get {total}.")
