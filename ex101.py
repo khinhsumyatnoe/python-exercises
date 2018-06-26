d7346d72d4dcabf13bc4b1df1daae0d15b7cc0f2
khinhsumyatnoe <khinsumyatnoe771997@gmail.com>
2018-06-26 11:56:27 +0630

    change

--- ex101.py
+++ ex101.py
@@ -1,12 +1,7 @@
 tabby_rat = "\tI'm tabbled in."
 persian_rat = "I'm split\non a line."
 backslash_rat = "I'm \\ a \\ cat."
-fat_rat="""
-I'll dona list:
-    \t*Cat food
-    \t*Fishes
-    \t*Catnip\n\t* Grass
-    """
+fat_rat="""I'll dona list: \t*Cat food \t*Fishes \t*Catnip\n\t* Grass"""
 
 print(tabby_rat)
 print(persian_rat)
